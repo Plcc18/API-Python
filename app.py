5edc8a6b73898806f34014198a4c7b896a1268e7
Plcc18 <pedrolucascabral4@gmail.com>
2024-11-30 17:49:12 -0300

    retorno de dados

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 #importações
-from flask import Flask #importando framework Flask
+from flask import Flask, request #importando framework Flask e função request
 from flask_sqlalchemy import SQLAlchemy #importando SQLAlchemy (biblioteca de ORM)
 
 app = Flask(__name__) #instância do Flask
@@ -8,11 +8,17 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///library.db' #configurando ban
 db = SQLAlchemy(app) #iniciando o banco de dados / conexão
 
 #modelando banco de dados
-class Product(db.Model):
+class Book(db.Model):
   id = db.Column(db.Integer, primary_key=True) #coluna de id (primary key)
   name = db.Column(db.String(120), nullable=False) #coluna de nome
   price = db.Column(db.Float, nullable=False) #coluna de preço
   description = db.Column(db.Text, nullable=True) #coluna de descrição (opcional)
+
+#definindo rota para adicionar livros
+@app.route('/api/books/add', methods=["POST"]) #local da rota e método
+def add_product():
+  data = request.json #dados enviados em json
+  return data #retorno de dados
 
 #definindo rota raiz e função executada ao acessá-la
 @app.route('/')
